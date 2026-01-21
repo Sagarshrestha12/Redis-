8272a1c4da0223e7cf5ffdbbc886ffc23aed3627
SagarShrestha12 <sagarshrestha4339@gmail.com>
2026-01-21 16:23:45 +1100

    implement for storing and getting the cache

--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -1,3 +1,16 @@
-export const getCachedPage = (route: string) => {};
+import {client } from '../redis/client';
 
-export const setCachedPage = (route: string, page: string) => {};
+const cacheRoutes = ['/about','/privacy','/auth/signin', '/auth/signup'];
+export const getCachedPage = (route: string) => {
+    if(cacheRoutes.includes(route)){
+        return client.get("page_cache#"+route);
+    }
+    return null;
+};
+
+
+export const setCachedPage = (route: string, page: string) => {
+    if(cacheRoutes.includes(route)){
+        return client.set("page_cache#"+route, page, {EX: 2});
+    }
+};
